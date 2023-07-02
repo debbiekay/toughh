f5f8a5543ca9d2778fb4ed629c99dd0de9a2be6a
Deborah <contactnkirukad@gmail.com>
2023-07-02 09:03:39 -0700

    commit

--- app.js
+++ app.js
@@ -1,18 +1,20 @@
-const firstContainer = document.querySelector(".firstcontainer")
-const secondContainer = document.querySelector(".secondContainer")
-const submitButton = document.getElementById("btn-submit")
-const rates = document.querySelectorAll(".btn")
-const rating = document.getElementById("rating")
+const firstContainer = document.querySelector(".firstcontainer");
+const secondContainer = document.querySelector(".secondContainer");
+const submitButton = document.getElementById("btn-submit");
+const rates = document.querySelectorAll(".btn");
+const rating = document.getElementById("rating");
+
 
 submitButton.addEventListener('click', () => {
-    firstContainer.classList.add('hidden');
+    firstContainer.style.display = "none";
     secondContainer.classList.remove('hidden')
 
    
 })
 
-rates.forEach((rate) =>{
-        rate.addEventListener('click', () =>{
-            rating.innerhtml = rate.innerhtml
+rates.forEach((rate) => {
+        rate.addEventListener('click', () => {
+            rating.innerHTML = rate.innerHTML
         })
     })
+
